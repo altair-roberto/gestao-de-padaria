c062de132aaa88936d6d86b714a6cc9c1acf0b92
Altair Roberto <altair.roberto.dev@gmail.com>
2018-10-11 19:31:56 -0400

    Remove uma classe desnecessario utilizando uma classe anonima
    A classe Aplicacao era muito simples, dessa forma pode ser transformada
    em uma classe anonima no unico lugar que a utilizava, o metodo
    SwingUtilities.invokeLater() na classe Principal.

--- Gestao de Padaria/src/gui/Principal.java
+++ Gestao de Padaria/src/gui/Principal.java
@@ -5,7 +5,14 @@ import javax.swing.SwingUtilities;
 public class Principal {
 
 	public static void main(String[] args) {
-		SwingUtilities.invokeLater(new Aplicacao());
+		SwingUtilities.invokeLater(new Runnable() {
+
+			@Override
+			public void run() {
+				new JanelaPrincipal("Gestor de Padaria");			
+			}
+			
+		});
 	}
 
 }
